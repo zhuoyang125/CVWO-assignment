47f3e3f76e0f574b9065c3844265ec39061e60af
Chan Zhuo Yang <zhuoyang125@gmail.com>
2021-01-30 19:58:36 +0800

    ie support

--- railsToDo/todo_frontend/src/index.js
+++ railsToDo/todo_frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import App from "./components/App";
+
 
 ReactDOM.render(
   <BrowserRouter>
