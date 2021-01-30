254ec58ecc95a4aa9bc1cf68ef5d92cd59710c15
Chan Zhuo Yang <zhuoyang125@gmail.com>
2021-01-30 20:19:10 +0800

    index.js change

--- railsToDo/todo_frontend/src/index.js
+++ railsToDo/todo_frontend/src/index.js
@@ -1,8 +1,8 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
 import App from "./components/App";
 
 
